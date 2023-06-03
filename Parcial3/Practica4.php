305d7c2273c55ebd3ade0cc4ab4cac5106ad5836
Carol Casas <124410526+CarolCasas@users.noreply.github.com>
2023-06-02 17:52:27 -0700

    02/06/23 Evaluacion con PHP (corregido)

--- Parcial3/Practica4.php
+++ Parcial3/Practica4.php
@@ -1,18 +1,19 @@
 <?php
 
     $nombre = $_POST["nombre"];
+    echo "<h3> Alumno: " .$nombre. "</h3>";
     $calificacion =0;
 
     $pregunta1 = $_POST["pregunta1"];
     $pregunta2 = $_POST["pregunta2"];
     $pregunta3 = $_POST["pregunta3"];
-    /*$pregunta4 = $_POST["pregunta4"];
+    $pregunta4 = $_POST["pregunta4"];
     $pregunta5 = $_POST["pregunta5"];
     $pregunta6 = $_POST["pregunta6"];
     $pregunta7 = $_POST["pregunta7"];
     $pregunta8 = $_POST["pregunta8"];
     $pregunta9 = $_POST["pregunta9"];
-    $pregunta10 = $_POST["pregunta10"];*/
+    $pregunta10 = $_POST["pregunta10"];
 
     echo "<h3>Pregunta 1, respuesta seleccionada: (".$pregunta1.")</h3>";
     if($pregunta1 == "a"){
@@ -37,7 +38,7 @@
         $calificacion ++;
         echo "<img src='correcto.png' width= '3%'><hr>";
     }else{
-        echo "<p> Respuesta es (a) </p>";
+        echo "<p> Respuesta es (b) </p>";
         echo "<img src='incorrecto.png' width= '3%'><hr>";
     }
 
@@ -46,7 +47,7 @@
         $calificacion ++;
         echo "<img src='correcto.png' width= '3%'><hr>";
     }else{
-        echo "<p> Respuesta es (a) </p>";
+        echo "<p> Respuesta es (b) </p>";
         echo "<img src='incorrecto.png' width= '3%'><hr>";
     }
 
@@ -55,7 +56,7 @@
         $calificacion ++;
         echo "<img src='correcto.png' width= '3%'><hr>";
     }else{
-        echo "<p> Respuesta es (a) </p>";
+        echo "<p> Respuesta es (c) </p>";
         echo "<img src='incorrecto.png' width= '3%'><hr>";
     }
 
@@ -64,7 +65,7 @@
         $calificacion ++;
         echo "<img src='correcto.png' width= '3%'><hr>";
     }else{
-        echo "<p> Respuesta es (a) </p>";
+        echo "<p> Respuesta es (b) </p>";
         echo "<img src='incorrecto.png' width= '3%'><hr>";
     }
 
@@ -73,7 +74,7 @@
         $calificacion ++;
         echo "<img src='correcto.png' width= '3%'><hr>";
     }else{
-        echo "<p> Respuesta es (a) </p>";
+        echo "<p> Respuesta es (c) </p>";
         echo "<img src='incorrecto.png' width= '3%'><hr>";
     }
 
@@ -82,7 +83,7 @@
         $calificacion ++;
         echo "<img src='correcto.png' width= '3%'><hr>";
     }else{
-        echo "<p> Respuesta es (a) </p>";
+        echo "<p> Respuesta es (c) </p>";
         echo "<img src='incorrecto.png' width= '3%'><hr>";
     }
 
@@ -91,7 +92,7 @@
         $calificacion ++;
         echo "<img src='correcto.png' width= '3%'><hr>";
     }else{
-        echo "<p> Respuesta es (a) </p>";
+        echo "<p> Respuesta es (b) </p>";
         echo "<img src='incorrecto.png' width= '3%'><hr>";
     }
 
@@ -105,13 +106,13 @@
     }
     
     if($calificacion >= 0 && $calificacion <=5){
-        echo "<h3> Calificación: ".$calificacion.", No aprovado (Informate bebé)</h3>";
+        echo "<h3> Calificación: ".$calificacion." <br> No aprovado (Informate bebé)</h3>";
         echo "<img src='Malilla.png' width'10%'<hr>";
     }else if($calificacion >=6 && $calificacion <=8){
-        echo "<h3> Calificación:".$calificacion.", Ahí la llevas (Tu puedes chikite)</h3>";
+        echo "<h3> Calificación: ".$calificacion." <br> Ahí la llevas (Tu puedes chikite)</h3>";
         echo "<img src='pensando.png' width'10%'<hr>";
     }else if($calificacion >=9 && $calificacion <=10){
-        echo "<h3> Calificación:".$calificacion.", Me impresionas (Que orgullo)</h3>";
+        echo "<h3> Calificación: ".$calificacion." <br> Me impresionas (Que orgullo)</h3>";
         echo "<img src='emocionade.png' width'10%'<hr>";
     }
 
